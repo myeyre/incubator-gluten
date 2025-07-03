09535d9f4947635f04c813cfc06d2fe3fec8a5bd
Eric Sun <eric@erimac2.lan>
2025-07-03 16:10:41 +0800

    BCLinux 8 build fix.

--- dev/package-vcpkg.sh
+++ dev/package-vcpkg.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-set -eux
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#set -eux
+set -ex
 
 CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 GLUTEN_DIR="$CURRENT_DIR/.."
@@ -9,6 +25,11 @@ VERSION=$(. /etc/os-release && echo ${VERSION_ID})
 ARCH=`uname -m`
 
 cd "$GLUTEN_DIR"
+if [ "$(uname -m)" = "aarch64" ]; then
+    export CPU_TARGET="aarch64"
+    export VCPKG_FORCE_SYSTEM_BINARIES=1
+fi
+
 if [ "$LINUX_OS" == "centos" ]; then
   if [ "$VERSION" == "8" ]; then
     source /opt/rh/gcc-toolset-11/enable
@@ -18,8 +39,12 @@ if [ "$LINUX_OS" == "centos" ]; then
   fi
 fi
 
-
 # build gluten with velox backend, prompt always respond y
 export PROMPT_ALWAYS_RESPOND=y
 
-./dev/buildbundle-veloxbe.sh --enable_vcpkg=ON --build_tests=ON --build_arrow=OFF --build_benchmarks=ON --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON "$@"
+export NUM_THREADS=1
+#./dev/buildbundle-veloxbe.sh --enable_vcpkg=ON --build_tests=ON --build_arrow=OFF --build_benchmarks=ON --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON "$@"
+#./dev/buildbundle-veloxbe.sh --enable_vcpkg=ON --build_tests=ON --build_arrow=ON --build_benchmarks=ON --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON "$@"
+./dev/buildbundle-veloxbe.sh --enable_vcpkg=ON --build_tests=OFF --build_arrow=OFF --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON "$@"
+#./dev/buildbundle-veloxbe.sh --enable_vcpkg=ON --build_tests=OFF --build_arrow=ON --build_benchmarks=OFF --enable_s3=ON --enable_gcs=ON --enable_hdfs=ON --enable_abfs=ON "$@"
+
