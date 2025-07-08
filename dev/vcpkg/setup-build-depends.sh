a70f049fd115dbce5589a535349520103bbf29d9
Eric Sun <eric@erimac2.lan>
2025-07-08 15:40:36 +0800

    BCLinux 8 build fix.

--- dev/vcpkg/setup-build-depends.sh
+++ dev/vcpkg/setup-build-depends.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 ## Install functions begin
@@ -129,6 +144,22 @@ install_centos_8() {
         flex bison python3 \
         java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
+    #yum -y install epel-release
+    #bclinux fix.
+    #https://docs.fedoraproject.org/en-US/epel/getting-started/#_el8
+    #subscription-manager repos --enable codeready-builder-for-rhel-8-$(arch)-rpms && dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    #https://developer.aliyun.com/mirror/epel
+    #yum install -y https://mirrors.aliyun.com/epel/epel-release-latest-8.noarch.rpm
+    #sed -i 's|^#baseurl=https://download.example/pub|baseurl=https://mirrors.aliyun.com|' /etc/yum.repos.d/epel*
+    #sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
+    yum -y install ccache
+
+    pip3 install --upgrade pip
+
+    # Requires cmake >= 3.28.3
+    pip3 install cmake==3.28.3
+
     dnf -y --enablerepo=powertools install autoconf-archive ninja-build
 
     install_maven_from_source
@@ -230,6 +261,9 @@ eval "$(sed -En "/^(VERSION_|)ID=/s/^/OS_/p" /etc/os-release)"
 
 [ -n "$OS_ID" -a -n "$OS_VERSION_ID" ] || log_fatal "Failed to detect os: ID or VERSION_ID is empty"
 
+install_bclinux_8.2() {
+    install_centos_8
+}
 INSTALL_FUNC="install_${OS_ID}_${OS_VERSION_ID}"
 [ "$(type -t "$INSTALL_FUNC")" == function ] || log_fatal "Unsupport OS: ${OS_ID} ${OS_VERSION_ID}"
 
