dac9613c8a99e46c6ecd3b38f78fdf28637d3075
Eric Sun <eric@erimac2.lan>
2025-07-15 10:33:30 +0800

    BCLinux 8 build fix.

--- dev/buildbundle-veloxbe.sh
+++ dev/buildbundle-veloxbe.sh
@@ -38,12 +38,14 @@ cd $GLUTEN_DIR
 
 check_supported
 
-# SPARK_VERSION is defined in builddeps-veloxbe.sh
-if [ "$SPARK_VERSION" = "ALL" ]; then
-  for spark_version in 3.2 3.3 3.4 3.5
-  do
-    build_for_spark $spark_version
-  done
-else
-  build_for_spark $SPARK_VERSION
-fi
+# # SPARK_VERSION is defined in builddeps-veloxbe.sh
+# if [ "$SPARK_VERSION" = "ALL" ]; then
+#   for spark_version in 3.2 3.3 3.4 3.5
+#   do
+#     build_for_spark $spark_version
+#   done
+# else
+#   build_for_spark $SPARK_VERSION
+# fi
+
+mvn clean package -Pbackends-velox -Pceleborn -Puniffle -Pspark-3.3 -Dhadoop.version=3.1.0 -DskipTests
